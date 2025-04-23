9eb31878b5737fa7ecaf283c60dad612765db16f
Grayson Butcher <graypbj@gmail.com>
2025-04-23 15:49:36 -0600

    Added chirps and logic to validate and clean them

--- main.go
+++ main.go
@@ -23,8 +23,10 @@ func main() {
 	mux.Handle("/app/", fsHandler)
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerCount)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerCount)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
